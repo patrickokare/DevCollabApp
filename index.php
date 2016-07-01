dfc419e5ba7d937928c5c79b20e9b40bd25cadc9
waleokare <waleokare@outlook.com>
2016-07-02 00:53:25 +0100

    dropbox

--- index.php
+++ index.php
@@ -15,6 +15,11 @@ require_once "dropbox-sdk/Dropbox/autoload.php";
 
 ?>
 
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<div class="g-hangout" data-render="createhangout"
+     data-initial_apps="[{ app_id : '123456789012', start_data : 'dQw4w9WgXcQ', 'app_type' : 'ROOM_APP' }]">
+</div>
+
 
 
 
