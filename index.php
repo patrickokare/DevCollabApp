637620830cd9625e8489fa89c3c351a22d150bcc
waleokare <waleokare@outlook.com>
2016-07-14 03:20:35 +0100

    Getting Page Structure In Place

--- index.php
+++ index.php
@@ -32,104 +32,6 @@
      data-initial_apps="[{ app_id : '123456789012', start_data : 'dQw4w9WgXcQ', 'app_type' : 'ROOM_APP' }]">
 </div>
 
-<div id="right">
-    <div id="wrapper">
-        <div id="menu">
-            <p class="welcome"> Logged in as: <b><?php echo $_SESSION['name']; ?></b></p>
-            <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
-            <div style="clear:both"></div>
-        </div>
-
-
-        <div id="chatbox"><?php
-            if(file_exists("log.html") && filesize("log.html") > 0){
-                $handle = fopen("log.html", "r");
-                $contents = fread($handle, filesize("log.html"));
-                fclose($handle);
-
-                echo $contents;
-            }
-            ?></div>
-
-<form name="message" action="">
-    <input name="usermsg" type="text" id="usermsg" size="63" />
-    <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
-</form>
-</div>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
-<script type="text/javascript">
-    // jQuery Document
-    $(document).ready(function(){
-
-    });
-</script>
-</div>
-
-<?
-session_start();
-
-function loginForm(){
-    echo'
-    <div id="loginform">
-    <form action="index.php" method="post">
-        <p>Please enter your name to continue:</p>
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" />
-        <input type="submit" name="enter" id="enter" value="Enter" />
-    </form>
-    </div>
-    ';
-}
-
-if(isset($_POST['enter'])){
-    if($_POST['name'] != ""){
-        $_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
-    }
-    else{
-        echo '<span class="error">Please type in a name</span>';
-    }
-
-}
-?>
-
-<script type="text/javascript">
-    // jQuery Document
-    $(document).ready(function(){
-        //If user wants to end session
-        $("#exit").click(function(){
-            var exit = confirm(" End this session????");
-            if(exit==true){window.location = 'index.php?logout=true';}
-        });
-    });
-</script>
-
-<script>
-<?
-if(isset($_GET['logout'])){
-
-//Simple exit message
-$fp = fopen("log.html", 'a');
-fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
-fclose($fp);
-
-session_destroy();
-header("Location: index.php"); //Redirect the user
-}
-
-?>
-
-//If user submits the form
-$("#submitmsg").click(function(){
-var clientmsg = $("#usermsg").val();
-$.post("post.php", {text: clientmsg});
-$("#usermsg").attr("value", "");
-return false;
-});
-
-
-</script>
-
-
 
 
 
@@ -138,6 +40,14 @@ return false;
 
 
 <br>
+
+
+
+
+
+
+
+
 
 
 
