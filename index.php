69d93774a40df7f4a4222e7e7917e6bb40aca2e2
waleokare <waleokare@outlook.com>
2016-07-14 03:42:24 +0100

    Getting Page Structure In Place

--- index.php
+++ index.php
@@ -153,7 +153,7 @@ else{
 <br>
 
 
-<a class="twitter-timeline"  href="https://twitter.com/hashtag/corgi" data-widget-id="753417739293908992">#corgi Tweets</a>
+<a class="twitter-timeline"  href="https://twitter.com/hashtag/MobileApplication" data-widget-id="753417739293908992">#MobileApplication Tweets</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
 
