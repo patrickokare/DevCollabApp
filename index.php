e3193fd95d25dc8a32bc2395223de1eb30255e05
waleokare <waleokare@outlook.com>
2016-07-14 02:56:28 +0100

    Getting Page Structure In Place

--- index.php
+++ index.php
@@ -41,7 +41,15 @@
         </div>
 
 
-<div id="chatbox"></div>
+        <div id="chatbox"><?php
+            if(file_exists("log.html") && filesize("log.html") > 0){
+                $handle = fopen("log.html", "r");
+                $contents = fread($handle, filesize("log.html"));
+                fclose($handle);
+
+                echo $contents;
+            }
+            ?></div>
 
 <form name="message" action="">
     <input name="usermsg" type="text" id="usermsg" size="63" />
@@ -93,7 +101,7 @@ if(isset($_POST['enter'])){
             if(exit==true){window.location = 'index.php?logout=true';}
         });
     });
-</script>
+
 <?
 if(isset($_GET['logout'])){
 
@@ -107,7 +115,7 @@ header("Location: index.php"); //Redirect the user
 }
 
 ?>
-<script>
+
 //If user submits the form
 $("#submitmsg").click(function(){
 var clientmsg = $("#usermsg").val();
@@ -115,41 +123,53 @@ $.post("post.php", {text: clientmsg});
 $("#usermsg").attr("value", "");
 return false;
 });
+
+
+
+//Load the file containing the chat log
+function loadLog(){
+
+$.ajax({
+url: "log.html",
+cache: false,
+success: function(html){
+$("#chatbox").html(html); //Insert chat log into the #chatbox div
+},
+});
+}
+
+
+
+    //Load the file containing the chat log
+    function loadLog(){
+        var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
+        $.ajax({
+            url: "log.html",
+            cache: false,
+            success: function(html){
+                $("#chatbox").html(html); //Insert chat log into the #chatbox div
+
+                //Auto-scroll
+                var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
+                if(newscrollHeight > oldscrollHeight){
+                    $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+                }
+            },
+        });
+    }
+
 </script>
 
 
 
 
+
+
+
+
+
 <br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+
 
 
 
