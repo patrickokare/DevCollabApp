8289b46e430cb556573f8f8fe538c34148dc326f
waleokare <waleokare@outlook.com>
2016-07-16 17:57:26 +0100

    working on the Chatting Feature

--- index.php
+++ index.php
@@ -148,7 +148,7 @@ else{
 
 </div>
 
-
+<!-- Implementing Ace API Code Snippet for the code editor -->
 <div id="editor">some text</div>
 <script src="src/ace.js" type="text/javascript" charset="utf-8"></script>
 <script>
