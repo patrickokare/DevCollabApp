aa3f52f79ecaded7b1448b0716a92ed5cc69d21c
waleokare <waleokare@outlook.com>
2016-07-16 19:17:30 +0100

    Code editor Implementation

--- index.php
+++ index.php
@@ -1,39 +1,3 @@
-<?
-session_start();
-
-if(isset($_GET['logout'])){
-
-    //Simple exit message
-    $fp = fopen("log.html", 'a');
-    fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
-    fclose($fp);
-
-    session_destroy();
-    header("Location: index.php"); //Redirect the user
-}
-
-function loginForm(){
-    echo'
-	<div id="loginform">
-	<form action="index.php" method="post">
-		<p>Enter the name you want to be identified among other users:</p>
-		<label for="name">Name:</label>
-		<input type="text" name="name" id="name" />
-		<input type="submit" name="enter" id="enter" value="Enter" />
-	</form>
-	</div>
-	';
-}
-
-if(isset($_POST['enter'])){
-    if($_POST['name'] != ""){
-        $_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
-    }
-    else{
-        echo '<span class="error">Please type in a name</span>';
-    }
-}
-?>
 
 
 
@@ -65,10 +29,10 @@ if(isset($_POST['enter'])){
 
 <textarea class ="codemirror-textarea"></textarea>
 
-<!-- JavaScript -->
- <script type="text/javascript" src="codemirror/js/jquery.min.js.js"</script>
+/** JavaScript for code editor **/
+<script type="text/javascript" src="codemirror/js/jquery.min.js.js"</script>
 
-<script type="text/javascript" src="codemirror/plugin/codemirror/lib/codemirror.js"> </script>
+<script type="text/javascript" src=""> </script>
 
 <script type="text/javascript" src="codemirror/plugin/codemirror/lib/codemirror.js"></script>
 
@@ -87,91 +51,6 @@ if(isset($_POST['enter'])){
 </div>
 
 
-<div id="left">
-
-    <?php
-    if(!isset($_SESSION['name'])){
-        loginForm();
-    }
-    else{
-        ?>
-        <div id="wrapper">
-            <div id="menu">
-                <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
-                <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
-                <div style="clear:both"></div>
-            </div>
-
-            <div id="chatbox">
-                <?php
-                if(file_exists("log.html") && filesize("log.html") > 0){
-                    $handle = fopen("log.html", "r");
-                    $contents = fread($handle, filesize("log.html"));
-                    fclose($handle);
-
-                    echo $contents;
-                }
-                ?>
-            </div>
-
-            <form name="message" action="">
-                <input name="usermsg" type="text" id="usermsg" size="63" />
-                <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
-            </form>
-        </div>
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
-        <script type="text/javascript">
-            // jQuery Document
-            $(document).ready(function(){
-                //If user submits the form
-                $("#submitmsg").click(function(){
-                    var clientmsg = $("#usermsg").val();
-                    $.post("post.php", {text: clientmsg});
-                    $("#usermsg").attr("value", "");
-                    return false;
-                });
-
-                //Load the file containing the chat log
-                function loadLog(){
-                    var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
-                    $.ajax({
-                        url: "log.html",
-                        cache: false,
-                        success: function(html){
-                            $("#chatbox").html(html); //Insert chat log into the #chatbox div
-                            var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
-                            if(newscrollHeight > oldscrollHeight){
-                                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
-                            }
-                        },
-                    });
-                }
-                setInterval (loadLog, 2500);	//Reload file every 2.5 seconds
-
-                //If user wants to end session
-                $("#exit").click(function(){
-                    var exit = confirm("Are you sure you want to end the session?");
-                    if(exit==true){window.location = 'index.php?logout=true';}
-                });
-            });
-        </script>
-        <?php
-    }
-    ?>
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -182,42 +61,8 @@ if(isset($_POST['enter'])){
 
 <br>
 <br>
-
-<div id="right">
-    <a class="twitter-timeline"  href="https://twitter.com/hashtag/MobileApplication" data-widget-id="753417739293908992">#MobileApplication Tweets</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <br>
 <br>
-
-
-
-
-
-
-
-
-
 
 
 
