3be287bd6b0df9d2ba02aeea5006cf2983feae7c
waleokare <waleokare@outlook.com>
2016-07-16 19:55:07 +0100

    Code editor Implementation finally!!!!!!!!!!!!!!!!!!!!!!!111yesss!

--- codemirror/js/default.js
+++ codemirror/js/default.js
@@ -1,28 +1,7 @@
-
-
 $(document).ready(function(){
-    var script_js = {
-        init: function(){
-            var code = document.getElementById("ed-ta");
-            var output = document.getElementById("op-iframe");
-            var editor = CodeMirror.fromTextArea(
-                code,
-                {
-                    lineNumbers: true,
-                    mode: "text/html",
-                    extraKeys: {
-                        "Ctrl-Space": "autocomplete"
-                    },
-                    indentUnit: 2
-                }
-            );
-            var btnrun = $("#btn-run");
-            btnrun.on("click", function(){
-                var v = editor.getValue();
-                output.contentDocument.write(v);
-                output.contentDocument.close();
-            });
-        }
-    };
-    script_js.init();
+
+    var code = $(".codemirror-textarea")[0];
+    var editor = CodeMirror.fromTextArea(code,{
+        lineNumbers : true
+    });
 });
