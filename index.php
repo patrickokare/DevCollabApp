eed12cec20ec0bd430484325155f42541493a894
waleokare <waleokare@outlook.com>
2016-07-16 20:37:09 +0100

    Code editor Implementation finally!!!!!!!!!!!!!!!!!!!!!!!111yesss!

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['preview-form-comment'
 
     <form id="preview-form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?> ">
 
-<textarea class="codemirror-textarea" name="preview-form-comment" id="preview-form-comment" rows="30" cols="80">
+<textarea class="codemirror-textarea" name="preview-form-comment" id="preview-form-comment" rows="20" cols="80">
     <?php echo $comment; ?>
 </textarea>
         <br>
