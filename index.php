9011bce21c5433c4a011ed2497d844d736bd9637
waleokare <waleokare@outlook.com>
2016-07-16 18:21:58 +0100

    Code editor Implementation

--- index.php
+++ index.php
@@ -74,26 +74,98 @@ if(isset($_POST['enter'])){
 </div>
 
 
+<div id="left">
+
+    <?php
+    if(!isset($_SESSION['name'])){
+        loginForm();
+    }
+    else{
+        ?>
+        <div id="wrapper">
+            <div id="menu">
+                <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
+                <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
+                <div style="clear:both"></div>
+            </div>
+
+            <div id="chatbox">
+                <?php
+                if(file_exists("log.html") && filesize("log.html") > 0){
+                    $handle = fopen("log.html", "r");
+                    $contents = fread($handle, filesize("log.html"));
+                    fclose($handle);
+
+                    echo $contents;
+                }
+                ?>
+            </div>
+
+            <form name="message" action="">
+                <input name="usermsg" type="text" id="usermsg" size="63" />
+                <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
+            </form>
+        </div>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+        <script type="text/javascript">
+            // jQuery Document
+            $(document).ready(function(){
+                //If user submits the form
+                $("#submitmsg").click(function(){
+                    var clientmsg = $("#usermsg").val();
+                    $.post("post.php", {text: clientmsg});
+                    $("#usermsg").attr("value", "");
+                    return false;
+                });
+
+                //Load the file containing the chat log
+                function loadLog(){
+                    var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
+                    $.ajax({
+                        url: "log.html",
+                        cache: false,
+                        success: function(html){
+                            $("#chatbox").html(html); //Insert chat log into the #chatbox div
+                            var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
+                            if(newscrollHeight > oldscrollHeight){
+                                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+                            }
+                        },
+                    });
+                }
+                setInterval (loadLog, 2500);	//Reload file every 2.5 seconds
+
+                //If user wants to end session
+                $("#exit").click(function(){
+                    var exit = confirm("Are you sure you want to end the session?");
+                    if(exit==true){window.location = 'index.php?logout=true';}
+                });
+            });
+        </script>
+        <?php
+    }
+    ?>
+
+
+
+
+
+
+
+
+
 
 </div>
 
-<!-- Implementing Ace API Code Snippet for the Code editor -->
-<div id="editor">some text</div>
-<script src="src/ace.js" type="text/javascript" charset="utf-8"></script>
-<script>
-    var editor = ace.edit("editor");
-</script>
 
-<script src="src/mode-javascript.js" type="text/javascript" charset="utf-8">
 
-var JavaScriptMode = ace.require("ace/mode/javascript").Mode;
-editor.session.setMode(new JavaScriptMode());
 
-</script>
-<script src="src/theme-twilight.js" type="text/javascript" charset="utf-8">
-editor.setTheme("ace/theme/twilight");
-</script>
-<!-- end -->
+
+
+
+
+
+
 
 
 
