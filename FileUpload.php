0a11bcc3d5f192b01e0638f7cf7a9f6c686b6888
waleokare <waleokare@outlook.com>
2016-07-20 22:58:26 +0100

    Debugging

--- FileUpload.php
+++ FileUpload.php
@@ -19,8 +19,11 @@ if(isset($_FILES['UploadFileField'])){
     if(!$UploadTmp){
         die("No File Selected, Please Upload Again");
     }else{
-        move_uploaded_file($UploadTmp, "Upload/$UploadName");
+        move_uploaded_file($UploadTmp, "C:\Users\user\PhpstormProjects\KareTechProject\Upload/$UploadName");
     }
+
+
+
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
