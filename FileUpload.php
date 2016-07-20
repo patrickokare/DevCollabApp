adf0c0fafcd13e1ad6d169cc8cdfd64d37f93850
waleokare <waleokare@outlook.com>
2016-07-20 23:30:39 +0100

    Debugging

--- FileUpload.php
+++ FileUpload.php
@@ -23,7 +23,6 @@ if(isset($_FILES['UploadFileField'])){
     }
 
 
-
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
