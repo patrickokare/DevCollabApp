df55e8c4a10dda4f4a8b8da57a776bc2f30c46e5
waleokare <waleokare@outlook.com>
2016-07-28 12:44:02 +0100

    xfghjk

--- upload.php
+++ upload.php
@@ -3,28 +3,27 @@
 if($_FILES['photo']['name'])
 {
 //if no errors...
-if(!$_FILES['photo']['error'])
-{
+if(!$_FILES['photo']['error']) {
 //now is the time to modify the future file name and validate the file
-$new_file_name = strtolower($_FILES['photo']['tmp_name']); //rename file
-if($_FILES['photo']['size'] > (1024000)) //can't be larger than 1 MB
-{
-$valid_file = false;
-$message = 'Oops!  Your file\'s size is to large.';
-}
+    $new_file_name = strtolower($_FILES['photo']['tmp_name']); //rename file
+    if ($_FILES['photo']['size'] > (1024000)) //can't be larger than 1 MB
+    {
+        $valid_file = false;
+        $message = 'Oops!  Your file\'s size is to large.';
+        echo $message;
+    }
 
 //if the file has passed the test
-if($valid_file)
-{
+    if ($valid_file) {
 //move it to where we want it to be
-move_uploaded_file($_FILES['photo']['tmp_name'], 'uploads/'.$new_file_name);
-$message = 'Congratulations!  Your file was accepted.';
-    echo $message;
+        move_uploaded_file($_FILES['photo']['tmp_name'], 'uploads/' . $new_file_name);
+        $message = 'Congratulations!  Your file was accepted.';
+        echo $message;
 
-    header('Location:  index.php');
-    exit();
-   // header("location:index.phplab");
-}
+        header("Location:index.php");
+        exit();
+
+    }
 }
 //if there is an error......
 else
