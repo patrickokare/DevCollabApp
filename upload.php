2d382cbffccd3df7bf237dfe904e8f11906ccbc3
waleokare <waleokare@outlook.com>
2016-07-28 12:26:03 +0100

    xfghjk

--- upload.php
+++ upload.php
@@ -1,29 +1,27 @@
 <?php
-$target_dir = "./Upload/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
+// Where the file is going to be placed
 
-        header('Location:  ./index.php');
-        exit();
-
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+/* Add the original filename to our target path.
+Result is "uploads/filename.extension" */
 
 
-    }
+$target_path = "Upload/";
 
+$target_path = $target_path . basename( $_FILES['uploadedfile']['name']);
 
+if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+    echo "The file ".  basename( $_FILES['uploadedfile']['name']).
 
+        " has been uploaded";
 
+    header('Location:  index.php');
+    exit();
+
+} else{
+    echo "There was an error uploading the file, please try again!";
 }
+
+
 
 
 
