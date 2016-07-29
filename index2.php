8fd84ea770fce43450b7cdfa62bee7b93e62ea12
waleokare <waleokare@outlook.com>
2016-07-29 02:36:51 +0100

    file upload test from new boston

--- index2.php
+++ index2.php
@@ -1,5 +1,32 @@
 <?php
 
-require 'start.php';
-require 'dropbox_auth.php';
+echo $name = $_FILES['file']['name'];
 
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name ="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Feature</title>
+</head>
+
+<body>
+
+
+<h3> Upload Your Files Here </h3>
+
+<form enctype="multipart/form-data" action="upload.php" method="post">
+
+    <input name="file" type="file" id="file" size="80">
+    <br>
+    <br>
+    <input type="submit" id="u_button" value="Upload your File">
+</form>
+
+
+
+</body>
