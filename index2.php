933cb0e1243f37de82a5eb921f3c3b3a50663b72
waleokare <waleokare@outlook.com>
2016-07-29 02:56:30 +0100

    file upload test from new boston

--- index2.php
+++ index2.php
@@ -11,15 +11,13 @@ echo $name = $_FILES['file']['name'];
 <head>
     <meta charset="UTF-8">
     <meta name ="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Feature</title>
+    <title>Test</title>
 </head>
 
 <body>
 
 
-
 <form action="upload.php" method="post" enctype="multipart/form-data">
-
     <input name="file" type="file">
     <br>
     <br>
