acdb8f1cc667cbd2539b56f45d215601453e643a
waleokare <waleokare@outlook.com>
2016-07-29 02:39:08 +0100

    file upload test from new boston

--- upload.php
+++ upload.php
@@ -1,23 +1,2 @@
-<?php
-if(isset($_FILES['UploadFileField'])){
-    // Creates the Variables needed to upload the file
-    $UploadName = $_FILES['UploadFileField']['name'];
-    $UploadName = mt_rand(100000, 999999).$UploadName;
-    $UploadTmp = $_FILES['UploadFileField']['tmp_name'];
-    $UploadType = $_FILES['UploadFileField']['type'];
-    $FileSize = $_FILES['UploadFileField']['size'];
-    // Removes Unwanted Spaces and characters from the files names of the files being uploaded
-    $UploadName = preg_replace("#[^a-z0-9.]#i", "", $UploadName);
-    // Upload File Size Limit
-    if(($FileSize > 125000)){
-        die("Error - File too Big");
-    }
-    // Checks a File has been Selected and Uploads them into a Directory on your Server
-    if(!$UploadTmp){
-        die("No File Selected, Please Upload Again");
-    }else{
-        move_uploaded_file($UploadTmp, "uploads/$UploadName");
-    }
-}
-?>
+
 
