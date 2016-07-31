5bc7b60b39716cf1a3f668259f3dc7b5ecde63b5
waleokare <waleokare@outlook.com>
2016-07-31 02:37:37 +0100

    Editing ChatFac chatbox desgin

--- ChatFac/chatbox.php
+++ ChatFac/chatbox.php
@@ -2,14 +2,14 @@
 include("config.php");
 if(isset($_SESSION['user'])){
     ?>
-    <h2>Room For ALL</h2>
+    <h2 xmlns="http://www.w3.org/1999/html">Room For ALL</h2>
     <a style="right: 20px;top: 20px;position: absolute;cursor: pointer;" href="logout.php">Log Out</a>
     <div class='msgs'>
         <?include("msgs.php");?>
     </div>
     <form id="msg_form">
 
-        <input name="msg" size="30" type="text"/>
+        <textarea name="msg">  </textarea>
         <button>Send</button>
     </form>
     <?
