b98b16b57da04e7b99e09eeb75adf746f5e0763b
waleokare <waleokare@outlook.com>
2016-07-31 02:13:38 +0100

    Editing ChatFac chatbox desgin

--- ChatFac/msgs.php
+++ ChatFac/msgs.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name ="viewport" content="width=device-width, initial-scale=1.0">
-    <title> ChatBox</title>
-
-    <link href="style1.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-<div class="chatbox">
-    <div class="chatlogs">
-
-        <div class="chat">
-
-
 
 <?php
 
@@ -22,7 +6,22 @@ $sql=$dbh->prepare("SELECT * FROM messages");
 $sql->execute();
 while($r=$sql->fetch()){
 
-    echo "<div class='msg'
+    echo "
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name =\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title> ChatBox</title>
+
+    <link href=\"style1.css\" rel=\"stylesheet\" type=\"text/css\">
+</head>
+<body>
+<div class=\"chatbox\">
+    <div class=\"chatlogs\">
+
+<div class=\"chat\">
 
 title='{$r['posted']}'>
  <span class='name'>{$r['name']}
@@ -33,7 +32,21 @@ title='{$r['posted']}'>
 
 </div>
 
- </div>";
+ </div>
+  <div class=\"chat-form\">
+        <textarea> </textarea>
+        <button>Send</button>
+
+    </div>
+
+</div>
+
+</body>
+</html>
+
+
+ ";
+
 }
 if(!isset($_SESSION['user']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH'])=='xmlhttprequest'){
     echo "<script>window.location.reload()</script>";
@@ -41,7 +54,3 @@ if(!isset($_SESSION['user']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH'])=='
 
 ?>
 
-
-
-</body>
-</html>
