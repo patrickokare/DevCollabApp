ffe5665b8ddbe30d7aadd9e3a66766152f10b561
waleokare <waleokare@outlook.com>
2016-07-31 01:51:49 +0100

    Editing ChatFac chatbox desgin

--- ChatFac/chatbox.php
+++ ChatFac/chatbox.php
@@ -2,17 +2,50 @@
 include("config.php");
 if(isset($_SESSION['user'])){
     ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name ="viewport" content="width=device-width, initial-scale=1.0">
+    <title> ChatBox</title>
+
+    <link href="style1.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<div class="chatbox">
+    <div class="chatlogs">
+
+        <div class="chat">
+
+
     <h2>Chat Room</h2>
+
     <a style="right: 20px;top: 20px;position: absolute;cursor: pointer;" href="logout.php">Log Out</a>
-    <div class='msgs'>
-        <?include("msgs.php");?>
+
+            <p class="chat-message">
+
+                  <?include("msgs.php");
+
+        ?>
+
+            </p>
+
     </div>
-    <form id="msg_form">
+</div>
 
-        <input name="msg" size="30" type="text"/>
-        <button>Send</button>
 
-    </form>
+        <div class="chat-form">
+            <textarea> </textarea>
+            <button>Send</button>
+
+        </div>
+    </div>
+</body>
+</html>
+
+
     <?
 }
 ?>
