ba4f28707dbade233db96b1e33521acf0db3ec28
waleokare <waleokare@outlook.com>
2016-07-31 02:07:30 +0100

    Editing ChatFac chatbox desgin

--- ChatFac/msgs.php
+++ ChatFac/msgs.php
@@ -1,13 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name ="viewport" content="width=device-width, initial-scale=1.0">
+    <title> ChatBox</title>
+
+    <link href="style1.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="chatbox">
+    <div class="chatlogs">
+
+        <div class="chat">
+
+
+
 <?php
 
 include("config.php");
 $sql=$dbh->prepare("SELECT * FROM messages");
 $sql->execute();
 while($r=$sql->fetch()){
-    echo "<div class='msg' title='{$r['posted']}'><span class='name'>{$r['name']}</span> : <span class='msgc'>{$r['msg']}</span></div>";
+
+    echo "<div class='msg'
+
+title='{$r['posted']}'>
+ <span class='name'>{$r['name']}
+ </span> :
+  <p class=\"chat-message\"> {$r['msg']}
+
+</p>
+
+</div>
+
+ </div>";
 }
 if(!isset($_SESSION['user']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH'])=='xmlhttprequest'){
     echo "<script>window.location.reload()</script>";
 }
 
 ?>
+
+
+
+</body>
+</html>
