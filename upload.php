90b76c93ccd9bf63ece71cf46a3d4b94441e408f
waleokare <waleokare@outlook.com>
2016-08-01 23:21:10 +0100

    file upload reading

--- upload.php
+++ upload.php
@@ -1,16 +1,9 @@
 <?php
 
 
-$location = 'uploads/';
-$document = file_get_contents($location);
 
-$lines = explode("\n", $document);
 
-foreach($lines as $newline){
 
-    echo $newline . '<br>';
-
-}
 
 
 
