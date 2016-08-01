5aa379eafbcccc62a445ff6e95a0c9c66241653a
waleokare <waleokare@outlook.com>
2016-08-01 01:56:36 +0100

    Video conferencing

--- video.js
+++ video.js
@@ -2,4 +2,22 @@
     var video = document.getElementById('video'),
         vendorUrl=window.URL || window.webkitURL;
 
+    navigator.getMedia= navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+
+    //Capture Video
+    navigator.getMedia({
+        video:true,
+        audio:false
+
+
+    }, function(stream) {
+       console.log(stream);
+
+    } );
+
+
+
+
+
 })();
