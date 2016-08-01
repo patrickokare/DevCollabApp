0245f8025fad7b9c0879cc3f0a2ebaea550bd2aa
waleokare <waleokare@outlook.com>
2016-08-01 02:05:01 +0100

    Video conferencing

--- video.js
+++ video.js
@@ -2,15 +2,16 @@
     var video = document.getElementById('video'),
         vendorUrl=window.URL || window.webkitURL;
 
-    navigator.getMedia= navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    navigator.getMedia= navigator.getUserMedia
+        || navigator.webkitGetUserMedia
+        || navigator.mozGetUserMedia
+        || navigator.msGetUserMedia;
 
 
     //Capture Video
     navigator.getMedia({
         video:true,
         audio:false
-
-
     }, function(stream) {
 
     video.src=vendorUrl.createObjectURL(Stream);
@@ -22,6 +23,6 @@
         // error.code
 
 
-    } );
+    });
 
 })();
