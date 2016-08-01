8ffc899b93c406d02e403add9004b498750e0fbf
waleokare <waleokare@outlook.com>
2016-08-02 00:52:46 +0100

    file upload reading

--- index2.php
+++ index2.php
@@ -114,7 +114,7 @@ if(is_dir($path)){
 <body>
 
 <form action="index2.php" method="POST" enctype="multipart/form-data">
-    <input type="file" name="file"/>
+    <input type="file" name="file"/> <br>
     <input type="submit" name="submit" value="Submit"/>
 
 </form>
