1cfa1eea44b9fa00fa7910b0ed425e7ff7874c2a
waleokare <waleokare@outlook.com>
2016-08-02 23:11:49 +0100

    file upload day 2

--- upload.php
+++ upload.php
@@ -1,61 +1,33 @@
 <?php
 
 
-$filename = 'data2.txt';
-$file_path = "./";
-$file_fullpath = $file_path . $filename;
-//
-//
-if (!file_exists($file_fullpath)) {
-    header("HTTP/1.0 404 Not Found");
-    return;
-}
-//
-// get the mime type
-$finfo = finfo_open(FILEINFO_MIME_TYPE);
-$mimeType = finfo_file($finfo, $file_fullpath);
-//
-// calc values for header
-$size = filesize($file_fullpath);
-$time = date('r', filemtime($file_fullpath));
-$fm = @fopen($file_fullpath, 'rb');
-if (!$fm) {
-    header('HTTP/1.0 505 Internal server error');
-    return;
-}
-$begin = 0;
-$end = $size;
-if (isset($_SERVER['HTTP_RANGE'])) {
-    if (preg_match('/bytes=\h*(\d+)-(\d*)[\D.*]?/i', $_SERVER['HTTP_RANGE'], $matches)) {
-        $begin = intval($matches[0]);
-        if (!empty($matches[1])) $end = intval($matches[1]);
+// open the current directory
+$dhandle = opendir('.');
+// define an array to hold the files
+$files = array();
+
+if ($dhandle) {
+    // loop through all of the files
+    while (false !== ($fname = readdir($dhandle))) {
+        // if the file is not this file, and does not start with a '.' or '..',
+        // then store it for later display
+        if (($fname != '.') && ($fname != '..') &&
+            ($fname != basename($_SERVER['PHP_SELF']))) {
+            // store the filename
+            $files[] = (is_dir( "./$fname" )) ? "(Dir) {$fname}" : $fname;
+        }
     }
+    // close the directory
+    closedir($dhandle);
 }
-//
-// create http header
-if ($begin > 0 || $end < $size) {
-    header('HTTP/1.0 206 Partial Content');
-}else {
-    header('HTTP/1.0 200 OK');
+
+echo "<select name=\"file\">\n";
+// Now loop through the files, echoing out a new select option for each one
+foreach( $files as $fname )
+{
+    echo "<option>{$fname}</option>\n";
 }
-header('HTTP/1.0 200 OK');
-header("Content-Type: $mimeType");
-header('Cache-Control: public, must-revalidate, max-age=0');
-header('Pragma: no-cache');
-header('Accept-Ranges: bytes');
-header('Content-Length:' . ($end - $begin));
-header("Content-Range: bytes $begin-$end/$size");
-header("Content-Disposition: inline; filename=$filename");
-header("Content-Transfer-Encoding: binary\n");
-header("Last-Modified: $time");
-header('Connection: close');
-// output the file
-$cur = $begin;
-fseek($fm, $begin, 0);
-while (!feof($fm) && $cur < $end && (connection_status() == 0)) {
-    print fread($fm, min(1024 * 16, $end - $cur));
-    $cur+= 1024 * 16;
-}
+echo "</select>\n";
 
 
 
