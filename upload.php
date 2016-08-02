21b643053a9225525539b40bdacd86dd184fe526
waleokare <waleokare@outlook.com>
2016-08-02 01:06:18 +0100

    file upload reading

--- upload.php
+++ upload.php
@@ -2,15 +2,24 @@
 
 
 $path = "./";
-if(is_dir($path)){
+if(is_dir($path)) {
 
+    $dir_handle = opendir($path);
 
-}else{
-    echo 'Is not a directory';
+    while (($dir = readdir()) !== false) {
 
+        if (is_dir($dir)) {
+            echo "is dir: " . $dir . "<br>";
+
+        } elseif (is_file($dir)) {
+
+            echo "is file: " . $dir . "<br>";
+        }
+
+    }
+closedir( );
 
 }
-
 
 
 
