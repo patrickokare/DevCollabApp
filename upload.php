60878ee86b91453c78eb7861823360b312391b80
waleokare <waleokare@outlook.com>
2016-08-02 02:05:48 +0100

    moment of truth

--- upload.php
+++ upload.php
@@ -30,11 +30,11 @@ $path = ".//uploads/";
 $handle = opendir($path);
 
 while($file = readdir($handle)){
-    if(substr($file,0,1)!= " .") {
+
         echo " <img src = '$file' /> ";
         echo '<br>';
 
-    }
+
 }
 
 closedir($handle);
