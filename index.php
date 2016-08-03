b79ee686b08dc0f4281fe8986e521eaae2c8114a
waleokare <waleokare@outlook.com>
2016-08-03 19:02:18 +0100

    -- took out the hangout bit --

--- index.php
+++ index.php
@@ -146,10 +146,7 @@ if(isset($_POST['enter'])){
             <div class="panel-heading">
      <h3>  Start Hangout  </h3>
                 </div>
-            <script src="https://apis.google.com/js/platform.js" async defer></script>
-            <div class="g-hangout" data-render="createhangout"
-                 data-initial_apps="[{ app_id : '123456789012', start_data : 'dQw4w9WgXcQ', 'app_type' : 'ROOM_APP' }]">
-            </div>
+
             <div class="panel-heading">
 
           <h3> Upload Your Files Here </h3>
