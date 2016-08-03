b91ae4fb8705d3eaf6727cc642cf0c537c088eee
waleokare <waleokare@outlook.com>
2016-08-03 01:51:55 +0100

    file upload  to download, final try of PHP Script

--- upload.php
+++ upload.php
@@ -1,4 +1,40 @@
 
+<?php
+ignore_user_abort(true);
+set_time_limit(0); // disable the time limit for this script
+
+$path = ".//uploads/"; // change the path to fit your websites document structure
+
+$dl_file = preg_replace("([^\w\s\d\-_~,;:\[\]\(\).]|[\.]{2,})", '', $_GET['download_file']); // simple file name validation
+$dl_file = filter_var($dl_file, FILTER_SANITIZE_URL); // Remove (more) invalid characters
+$fullPath = $path.$dl_file;
+
+if ($fd = fopen ($fullPath, "r")) {
+    $fsize = filesize($fullPath);
+    $path_parts = pathinfo($fullPath);
+    $ext = strtolower($path_parts["extension"]);
+    switch ($ext) {
+        case "pdf":
+            header("Content-type: application/pdf");
+            header("Content-Disposition: attachment; filename=\"".$path_parts["basename"]."\""); // use 'attachment' to force a file download
+            break;
+        // add more headers for other content types here
+        default;
+            header("Content-type: application/octet-stream");
+            header("Content-Disposition: filename=\"".$path_parts["basename"]."\"");
+            break;
+    }
+    header("Content-length: $fsize");
+    header("Cache-control: private"); //use this to open files directly
+    while(!feof($fd)) {
+        $buffer = fread($fd, 2048);
+        echo $buffer;
+    }
+}
+fclose ($fd);
+exit;
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -7,42 +43,6 @@
     <meta name ="viewport" content="width=device-width, initial-scale=1.0">
     <title>Download</title>
 
-    <?php
-    ignore_user_abort(true);
-    set_time_limit(0); // disable the time limit for this script
-
-    $path = ".//uploads/"; // change the path to fit your websites document structure
-
-    $dl_file = preg_replace("([^\w\s\d\-_~,;:\[\]\(\).]|[\.]{2,})", '', $_GET['download_file']); // simple file name validation
-    $dl_file = filter_var($dl_file, FILTER_SANITIZE_URL); // Remove (more) invalid characters
-    $fullPath = $path.$dl_file;
-
-    if ($fd = fopen ($fullPath, "r")) {
-        $fsize = filesize($fullPath);
-        $path_parts = pathinfo($fullPath);
-        $ext = strtolower($path_parts["extension"]);
-        switch ($ext) {
-            case "pdf":
-                header("Content-type: application/pdf");
-                header("Content-Disposition: attachment; filename=\"".$path_parts["basename"]."\""); // use 'attachment' to force a file download
-                break;
-            // add more headers for other content types here
-            default;
-                header("Content-type: application/octet-stream");
-                header("Content-Disposition: filename=\"".$path_parts["basename"]."\"");
-                break;
-        }
-        header("Content-length: $fsize");
-        header("Cache-control: private"); //use this to open files directly
-        while(!feof($fd)) {
-            $buffer = fread($fd, 2048);
-            echo $buffer;
-        }
-    }
-    fclose ($fd);
-    exit;
-
-    ?>
 </head>
 
 <body>
@@ -56,6 +56,12 @@
 
 </body>
 </html>
+
+
+
+
+
+
 
 
 
