b076d1a4b835ce49b5fd6aca8f2d184348b6ab65
waleokare <waleokare@outlook.com>
2016-08-05 23:32:51 +0100

    Audio/Video Conference practice

--- hangout.php
+++ hangout.php
@@ -1,4 +1,21 @@
-<?php
 
-echo 'working!!!';
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name ="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet" type="text/css">
 
+    <script src ="js/jquery.js"> </script>
+    <script src="js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+<iframe src="https://appear.in/karetechChatRoom" width=700" height="600" frameborder="0"></iframe>
+
+</body>
+</html>
