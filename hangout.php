11f176688fd68727d04debe28daa2a152b7ed360
waleokare <waleokare@outlook.com>
2016-08-06 00:42:32 +0100

    webRTC

--- hangout.php
+++ hangout.php
@@ -7,18 +7,52 @@
     <title>Contact</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet" type="text/css">
-
+    <script src="https://simplewebrtc.com/latest-v2.js"></script>
     <script src ="js/jquery.js"> </script>
     <script src="js/bootstrap.min.js"></script>
 
 </head>
 
 <body>
+       <div class="videoContainer">
+
+<script>
+
+    var webrtc = new SimpleWebRTC({
+        localVideoEl: 'localVideo',
+        remoteVideosEl: '' // empty string
+    });
+
+    webrtc.on('videoAdded', function (video, peer) {
+        console.log('video added', peer);
+        var remotes = document.getElementById('remotes');
+        if (remotes) {
+            var container = document.createElement('div');
+            container.className = 'videoContainer';
+            container.id = 'container_' + webrtc.getDomId(peer);
+            container.appendChild(video);
+
+            // suppress contextmenu
+            video.oncontextmenu = function () { return false; };
+
+            remotes.appendChild(container);
+        }
+    });
+    // a peer video was removed
+    webrtc.on('videoRemoved', function (video, peer) {
+        console.log('video removed ', peer);
+        var remotes = document.getElementById('remotes');
+        var el = document.getElementById(peer ? 'container_' + webrtc.getDomId(peer) : 'localScreenContainer');
+        if (remotes && el) {
+            remotes.removeChild(el);
+        }
+    });
 
 
 
 
-
+</script>
+</div>
 
 
 </body>
