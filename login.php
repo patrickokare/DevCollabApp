ad573b92196d411a97915a41e3265811b4a16a89
waleokare <waleokare@outlook.com>
2016-08-05 01:11:58 +0100

    Designing Of the Login Page

--- login.php
+++ login.php
@@ -1,29 +1,31 @@
 <?php
 include ("connection.php");
 
-
-if(empty($_POST["username"]) || empty($_POST["password"])){
-
-    echo "Both fields are required. ";
-
-}else{
-
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-
-    $sql = " SELECT uid
-         FROM  users
-         WHERE username ='$username'
-         and  password ='$password'";
-
-    $result = mysqli_query($db, $sql);
-
-    if(mysqli_num_rows($result) == 1) {
+session_start();
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    // username and password sent from form
+    $username = mysqli_real_escape_string($dbs,$_POST['username']);
+    $password = mysqli_real_escape_string($dbs,$_POST['password']);
+    $sql = "SELECT uid
+            FROM users
+            WHERE username = '$username'
+            and
+            password = '$password'";
+    $result = mysqli_query($dbs,$sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    $active = $row['active'];
+    $count = mysqli_num_rows($result);
+    // If result matched $username and $password, table row must be 1 row
+    if($count == 1) {
+//error check here...........
+        $_SESSION['login_user'] = $username;
+        echo $_SESSION['login_user'];
         header("location: Home.php?username=$username"); // Redirecting To another Page
-    }else{
-        echo "Incorrect username or password.";
+    }else {
+        $error = 'Your Login Name or Password is invalid';
+        echo 'Your Login Name or Password is invalid';
     }
-
-
+}
+if(empty($_POST["username"]) || empty($_POST["password"])) {
+    echo "Both fields are required. ";
 }
