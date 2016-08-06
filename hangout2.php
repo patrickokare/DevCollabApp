069f6834ba0722ec849b1694376353572ade11f6
waleokare <waleokare@outlook.com>
2016-08-06 02:43:43 +0100

    Hangout2 testing the button element.

---
+++ hangout2.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name ="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Page </title>
+<script src="http://simplewebrtc.com/latest.js"></script>
+
+<style>
+    #remoteVideos video {
+        height: 250px;
+
+        color:  darkred;
+    }
+    #localVideo {
+        height: 250px;
+        float: right;
+
+        color:  #066;
+
+    }
+</style>
+</head>
+<body>
+
+<h1> Test 24 </h1>
+
+
+<video id="localVideo" height="300">
+
+    </video>
+
+<div id="remotesvideos">
+    </div>
+<br>
+
+<button id="btn1" onclick="startconf()">Start Conferences
+
+
+</button>
+
+<script language="javascript">
+    var webrtc = null;
+
+    function startconf() {
+
+        webrtc = new SimpleWebRTC({
+            localVideoEl: 'localVideo',
+            // the id/element dom element that will hold remote videos
+            remoteVideosEl: 'remoteVideos',
+            // immediately ask for camera access
+            autoRequestMedia: true
+        });
+
+        // we have to wait until it's ready
+        webrtc.on('readyToCall', function () {
+            // you can name it anything
+            webrtc.joinRoom('KaretechChatRoom86');
+        });
+
+
+    };
+</script>
+
+
+</body>
+</html>
