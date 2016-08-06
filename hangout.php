3b8e680a8011e77fb872f938339c4083b5d3e28d
waleokare <waleokare@outlook.com>
2016-08-06 01:14:50 +0100

    webRTC

--- hangout.php
+++ hangout.php
@@ -4,12 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name ="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
+    <title>Hangout</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet" type="text/css">
     <script src="https://simplewebrtc.com/latest-v2.js"></script>
     <script src ="js/jquery.js"> </script>
     <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="codemirror/js/jquery.min.js.js"></script>
+    <script type="text/javascript" src="codemirror/plugin/codemirror/lib/codemirror.js"></script>
+    <script type="text/javascript" src ="codemirror/js/default.js"></script>
 
 </head>
 
@@ -30,6 +33,14 @@
         // immediately ask for camera access
         autoRequestMedia: true
 
+
+    });
+        // we have to wait until it's ready
+        webrtc.on('readyToCall', function () {
+        // you can name it anything
+        webrtc.joinRoom('chat');
+
+
     });
 
 
@@ -49,7 +60,15 @@
 
             remotes.appendChild(container);
         }
-    });
+        var vol = document.createElement('meter');
+        vol.id = 'volume_' + peer.id;
+        vol.className = 'volume';
+        vol.min = -45;
+        vol.max = -20;
+        vol.low = -40;
+        vol.high = -25;
+        container.appendChild(vol);
+
     // show the ice connection state
     if (peer && peer.pc) {
         var connstate = document.createElement('div');
@@ -88,6 +107,44 @@
             remotes.removeChild(el);
         }
     });
+
+    // local p2p/ice failure
+    webrtc.on('iceFailed', function (peer) {
+        var connstate = document.querySelector('#container_' + webrtc.getDomId(peer) + ' .connectionstate');
+        console.log('local fail', connstate);
+        if (connstate) {
+            connstate.innerText = 'Connection failed.';
+            fileinput.disabled = 'disabled';
+        }
+    });
+
+    // remote p2p/ice failure
+    webrtc.on('connectivityError', function (peer) {
+        var connstate = document.querySelector('#container_' + webrtc.getDomId(peer) + ' .connectionstate');
+        console.log('remote fail', connstate);
+        if (connstate) {
+            connstate.innerText = 'Connection failed.';
+            fileinput.disabled = 'disabled';
+        }
+    });
+
+
+    function showVolume(el, volume) {
+        console.log('showVolume', volume, el);
+        if (!el) return;
+        if (volume < -45) volume = -45; // -45 to -20 is
+        if (volume > -20) volume = -20; // a good range
+        el.value = volume;
+    }
+
+// local volume has changed
+        webrtc.on('volumeChange', function (volume, treshold) {
+            showVolume(document.getElementById('localVolume'), volume);
+        });
+        // remote volume has changed
+        webrtc.on('remoteVolumeChange', function (peer, volume) {
+            showVolume(document.getElementById('volume_' + peer.id), volume);
+        });
 
            </script>
 
