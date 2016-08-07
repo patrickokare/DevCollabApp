633f562f292d3e8866b7042a64478812ec4146cb
waleokare <waleokare@outlook.com>
2016-08-07 14:53:49 +0100

    Register form

--- register.php
+++ register.php
@@ -4,7 +4,25 @@ require 'session.php';
 
 if(!loggedin()){
 
-echo 'Register First.';
+?>
+<form action ="register.php" method="POST">
+
+    <label> UserName
+    <input type="text" name="username">
+    </label><br> <br>
+    <label> Password
+    <input type="password" name="password">
+    </label><br> <br>
+    <label> Password Again
+    <input type="password" name="password_again">
+    </label>
+    <br> <br>
+     <input type="submit" value="Register">
+
+</form>
+
+
+<?php
 
 } else if (loggedin()) {
     echo 'you are already registered and logged in. ';
