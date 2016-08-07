6aba52522bd77f81ccd42e959ede1a2cffbf9e1d
waleokare <waleokare@outlook.com>
2016-08-07 22:17:06 +0100

    More validation on the Registration form.

--- success.php
+++ success.php
@@ -1,14 +1,11 @@
-<?php
-session_start();
-if(!isset($_SESSION['username'])){
-    header("location:register.php");
-}
-?>
+
 
 <?php
 
 $username = $_GET['username'];
 ?>
+
+
 
 <!DOCTYPE html>
 <html>
