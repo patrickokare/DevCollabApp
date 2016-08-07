c39baaf1b6cad7b6633d43cf6eb416f20289566a
waleokare <waleokare@outlook.com>
2016-08-07 15:35:27 +0100

    Register form

--- register.php
+++ register.php
@@ -16,7 +16,7 @@ if(loggedin()) {
             echo 'OKAY.';
         } else {
 
-            echo '<Span style="color: darkred"> All fields are required.</Span>';
+            echo '<Span style="color: darkred"> All Fields are required.</Span>';
         }
     }
 
@@ -26,24 +26,86 @@ if(loggedin()) {
 
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name ="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src ="js/jquery.js"> </script>
+    <script src="js/bootstrap.min.js"></script>
 
+</head>
 
+<body>
+
+<header>
+    <nav class="navbar navbar-default navbar-static-top no-margin" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-ArrayTech-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="Home.php"> <em style="font-family: cursive"> DevCollab  </em> </a>
+            </div>
+
+        </div>
+    </nav>
+</header>
+
+<div class="container">
 
 <form action ="register.php" method="POST">
 
     <label> UserName:<br> <br>
-    <input type="text" name="username">
+    <input type="text" name="username" value="<?php echo $username; ?>">
     </label><br> <br>
     <label> Password:<br> <br>
-    <input type="password" name="password">
+    <input type="password" name="password" value="<?php echo $username; ?>">
     </label><br> <br>
     <label> Password Again:<br> <br>
-    <input type="password" name="password_again">
+    <input type="password" name="password_again" value="<?php echo $username; ?>">
     </label> <br> <br>
 
      <input type="submit" value="Register">
 
 </form>
 
+</div>
 
+<footer class="site-footer">
+
+    <div class="container">
+        <div class="row">
+
+            <span style="font-family: cursive" class="moveright"> DevCollab App(c) </span>
+
+
+
+        </div>
+        <div class="bottom-footer">
+            <div class="col-md-5"> Designed by Wale Patrick 2016(c)</div>
+            <div class="col-md-7">
+
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+</footer>
+
+</body>
+</html>
 
