0bbd88129781d585bd81fbe3843621b9bf5fa657
waleokare <waleokare@outlook.com>
2016-08-09 04:44:31 +0100

    code mirror coding

--- default.js
+++ default.js
@@ -2,14 +2,11 @@ $(document).ready(function(){
 
     var code = $(".codemirror-textarea")[0];
 
-    var editor = CodeMirror.from(code,{
-        lineNumbers : true
-    });
-  /**
+
+
     var editor = CodeMirror.fromTextArea(code,{
         lineNumbers : true
     });
-**/
 
     $("#preview-form").submit(function(e){
         var value = editor.getValue();
