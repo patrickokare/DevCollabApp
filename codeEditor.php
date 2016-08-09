3f531e67c44fb1405d6aa6b9104573183eee7987
waleokare <waleokare@outlook.com>
2016-08-09 03:02:31 +0100

    Code editor implementing JS

---
+++ codeEditor.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name ="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Page </title>
+    <script src="lib/codemirror.js"> </script>
+    <link rel="stylesheet" href="lib/codemirror.css">
+    <script src="mode/javascript/javascript.js"></script>
+    <link rel="stylesheet" href="theme/night.css">
+    <script src="hint/show-hint.js"></script>
+    <script src="hint/css-hint.js"></script>
+    <link rel="stylesheet" href="hint/show-hint.css">
+
+    <style>
+        body{
+            background-color: greenyellow;
+        }
+
+
+
+    </style>
+</head>
+<body>
+<form id="codeeditor" style="width: 60%"></form>
+
+
+
+
+<script>
+
+    var editor = CodeMirror(document.getElementById("codeeditor"),{
+
+        mode: "javascript",
+        theme: "night",
+        tabSize: 20,
+        lineNumbers: true,
+        firstLineNumber:1,
+        extraKeys:{"Ctrl": "autocomplete"}
+    });
+
+</script>
+
+
+</body>
+</html>
+
