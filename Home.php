cc83e46e94ba075d5ea70bbe0e5d6cd1ace2e9ae
waleokare <waleokare@outlook.com>
2016-08-09 05:28:40 +0100

    code mirror coding

--- Home.php
+++ Home.php
@@ -103,15 +103,7 @@ if(!isset($_SESSION['login_user'])){
                 <h3>  Code Editor  </h3>
             </div>
 
-            <h2>HEllo helogrdshhhhssssssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhhhhsssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhh</h2>
-            <h2>HEllo helogrdshhhhhhhhhssssssssssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhhhsssssssssssssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhhhhsssssssssssssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhhhhssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhssssssssssssss</h2>
+            <?php include ("codeEditor.php")?>
 
     </div>
 </div>
