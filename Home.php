3577addd2b8a899f0faaf44b596ee6ee8f8cd751
waleokare <waleokare@outlook.com>
2016-08-09 05:20:25 +0100

    code mirror coding

--- Home.php
+++ Home.php
@@ -103,16 +103,16 @@ if(!isset($_SESSION['login_user'])){
                 <h3>  Code Editor  </h3>
             </div>
 
-            <h2>HEllo helogrdshhhhhhhhhhhsssssssssssssssssssssssssssssssssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhhhhsssssssssssssssssssssssssssssssssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssssssssssssssssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssssssssssssssssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssssssssssssssssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssssssssssssssssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssssssssssssssssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssssssssssssssssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssssssssssssssssssssssssss</h2>
-            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssssssssssssssssssssssssss</h2>
+            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssssssssssssssss</h2>
+            <h2>HEllo helogrdshhhhhhhhhhhsssssssssssssssssssssssssssssssss</h2>
+            <h2>HEllo helogrdshhhhhhhhhhhsssssssssssssssssssssssssssssss</h2>
+            <h2>HEllo helogrdshhhhhhhhhhhsssssssssssssssssssssssssssssssssss</h2>
+            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssssssssssssssssss</h2>
+            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssssssssssssssss</h2>
+            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssssssssssssssss</h2>
+            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssssssssssssssss</h2>
+            <h2>HEllo helogrdshhhhhhhhhhhssssssssssssssssssssssssssssssssss</h2>
+            <h2>HEllo helogrdshhhhhhhhhhhsssssssssssssssssssssssssssssss</h2>
 
     </div>
 </div>
