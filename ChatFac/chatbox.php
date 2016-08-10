77ae0d01d76b074aa57fb8fc132bb9b17efa0a9c
waleokare <waleokare@outlook.com>
2016-08-11 00:25:40 +0100

    Changing the codes in chat box

--- ChatFac/chatbox.php
+++ ChatFac/chatbox.php
@@ -3,11 +3,15 @@ include("config.php");
 if(isset($_SESSION['login_user'])){
     ?>
     <h2>Chat Session</h2>
+  <!--
     <a style="right: 20px;top: 20px;position: absolute;cursor: pointer;" href="messgLogout.php">Log Out</a>
+    -->
     <div class='msgs'>
+
         <?include("msgs.php");?>
     </div>
     <form id="msg_form">
+
         <input name="msg" size="40" type="text" height="20"/>
 
         <br>
