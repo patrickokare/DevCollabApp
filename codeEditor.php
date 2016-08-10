11471384c23a69d830fbe7aa49c703ed5f2665ce
waleokare <waleokare@outlook.com>
2016-08-10 01:12:30 +0100

    Try and error code mirror

--- codeEditor.php
+++ codeEditor.php
@@ -1,12 +1,26 @@
 <?php
-
+$path = '/usercodes/';
 $comment = null;
 // when the form is submitted this code below will run
-if($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['preview-form-comment'])){
+if($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['preview-form-comment'])) {
     $comment = $_POST['preview-form-comment'];
 
-}
+    if ($_POST['type'] == 'save') {
 
+        $content = stripslashes($_POST['code']);
+
+        $username = addslashes(strip_tags($_POST['username'])); //i.e. markrummel
+
+        $filename = addslashes(strip_tags($_POST['filename'])); //i.e. test, index
+
+        $ext = addslashes(strip_tags($_POST['filetype'])); //i.e. html, css, js
+
+        $path = '/usercodes/' . $username . '/';
+        $URL = $path . $filename . '.' . $ext;
+
+        file_put_contents($URL, $content);
+    }
+}
 ?>
 
 <!DOCTYPE html>
