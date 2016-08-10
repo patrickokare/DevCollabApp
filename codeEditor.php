7fb7c0e917326bb300f924873f768de1f03e2d7c
waleokare <waleokare@outlook.com>
2016-08-10 02:09:24 +0100

    Code Mirror writing a file

--- codeEditor.php
+++ codeEditor.php
@@ -7,7 +7,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['preview-form-comment'
 
 
     $content = $comment;
-    $fp = fopen($_SERVER['.//uploads/'] . "/myText.txt","wb");
+    $fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/myText.txt","wb");
     fwrite($fp,$content);
     fclose($fp);
 
@@ -75,9 +75,9 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['preview-form-comment'
 
                  </textarea>
 
-<a href="<?php echo $_SERVER['.//upload/'].'myText.txt'; ?>"></a>
+<a href="<?php echo $_SERVER['DOCUMENT_ROOT'].'usercodes.txt'; ?>"></a>
 <br />
-<a href="/myText.txt">go to with html</a>
+<a href="/usercodes.txt">Click to get your Code Results.</a>
 
 
 </body>
