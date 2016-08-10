98c4f22b2ef5f87e924cf3e4520fd7af6bc4251f
waleokare <waleokare@outlook.com>
2016-08-11 00:15:10 +0100

    Changing the codes in chat box

--- ChatFac/messglogin.php
+++ ChatFac/messglogin.php
@@ -10,16 +10,22 @@ if(isset($_POST['name']) && !isset($display_case)){
     $sql->execute(array($name));
 
     if($sql->rowCount()!=0){
-        $ermsg= "<h2 class='error'>Name Taken. <a href='messgIndex.php'>Please try another Name.</a></h2>";
+
+        $ermsg= " <h2 class='error'>Name Taken. <a href='messgIndex.php'>Try another Name.</a></h2>";
+
     }else{
         $sql=$dbh->prepare("INSERT INTO chatters (name,seen) VALUES (?,NOW())");
+
         $sql->execute(array($name));
+
         $_SESSION['login_user']=$name;
+
     }
 
 }elseif(isset($display_case)){
 
     if(!isset($ermsg)){
+
         ?>
 
 
@@ -39,7 +45,7 @@ if(isset($_POST['name']) && !isset($display_case)){
 
            <form action="messgIndex.php" method="POST">
                <div class="form-input">
-                   <div style="color: whitesmoke">Enter Chat Name  <input name="name" placeholder="Chat Name"/></div>
+                   <div style="color: whitesmoke"> Enter Chat Name  <input name="name" placeholder="Chat Name"/></div>
                </div>
 
             <button class="btn-submit">Start Chatting</button>
