6cd9573709a798a44543ab1b989a3330e07b89de
waleokare <waleokare@outlook.com>
2016-08-11 02:57:57 +0100

    creating new chat box deleting old one

--- Chat/index.php
+++ Chat/index.php
@@ -8,24 +8,25 @@
     <meta charset="UTF-8">
 
     <title>Chat Box </title>
+
 <script>
     function submitChat(){
         if (form1.uname.value == '' || form1.msg.value == ''){
-            alert('ALL FIELDS ARE REQUIRED!!!!');
+            alert('ALL FIELDS ARE MANDATORY!!!!');
             return;
         }
         var uname = form1.uname.value;
-        var msg = form.msg.value;
+        var msg = form1.msg.value;
         var xmlhttp = new XMLHttpRequest();
 
         xmlhttp.onreadystatechange = function(){
-            if(xmlhttp.readyState ==4&&xmlhttp.status==200) {
+            if(xmlhttp.readyState ==4&&xmlhttp.status==200){
                 document.getElementById('chatlogs').innerHTML = xmlhttp.responseText;
             }
 
-
         }
-        xmlhttp.open('GET', 'insert.php?uname='+uname+'&msg='+msg, true);
+
+        xmlhttp.open('GET', 'insert.php?uname='+uname+'&msg='+msg,true);
         xmlhttp.send();
     }
 
--- Chat/insert.php
+++ Chat/insert.php
@@ -5,12 +5,6 @@ $msg = $_REQUEST['msg'];
 
 include ("../connection.php");
 
-if (mysqli_connect_errno())
-{
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-} else{
-    echo 'successful';
-}
 
 mysqli_select_db($db,'chatbox');
 
