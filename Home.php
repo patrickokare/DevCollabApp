eb9b46f553b348c75630f6d561f4e56fd4f2da7b
waleokare <waleokare@outlook.com>
2016-08-11 21:36:17 +0100

    Seach the name of the file you want to edit

--- Home.php
+++ Home.php
@@ -193,7 +193,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['preview-form-comment'
                           <option selected="selected">Choose one</option>
                           <?php
 
-                          foreach($names as $name) { ?>
+                          foreach($name as $names) { ?>
                               <option value="<?= $name['name'] ?>"><?= $name['name'] ?></option>
                               <?php
                           } ?>
