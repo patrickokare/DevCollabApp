9c8e296ef49b6f0b8ebb99280f555690779d56cc
waleokare <waleokare@outlook.com>
2016-08-12 21:15:37 +0100

    Using php blank

--- Home.php
+++ Home.php
@@ -26,6 +26,10 @@ if(!isset($_SESSION['login_user'])){
 $userfilename = $_SESSION['login_user'].'_'.'codes.txt';
 $oldfile = file_get_contents("http://karetechapp.azurewebsites.net/usercodes/".$userfilename);
 
+$link =  window.open("http://karetechapp.azurewebsites.net/usercodes/".$userfilename);
+
+//echo $link;
+
 $comment = $oldfile;
 
 // when the form is submitted this code below will run
