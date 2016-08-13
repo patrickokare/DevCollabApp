203dff02450fff9b8d3608a5785b1e8cd76bd7be
waleokare <waleokare@outlook.com>
2016-08-13 01:49:22 +0100

    Adding more security to the Login Page

--- login.php
+++ login.php
@@ -17,8 +17,12 @@ session_start();
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     // username and password sent from form
+    // To protect MySQL injection for Security purpose
     $username = mysqli_real_escape_string($db,$_POST['username']);
     $password = mysqli_real_escape_string($db,$_POST['password']);
+
+    $username = stripslashes($username);
+    $password = stripslashes($password);
 
     $sql = "SELECT uid
             FROM users
@@ -37,21 +41,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
          echo $_SESSION['login_user'];
       //  header("location: Home.php?username=$username");
-
         header("location: Home.php");// Redirecting To another Page
 
-        }else if($count == 0) {
-
+        }else {
         header("location: index.php");
 
         $error =  'username or password incorrect';
-      //  $error = 'YOUR PASSWORD IS NOT RECOGNIZED! TRY AGAIN... <br>';
         echo $error;
     }
 }
+mysqli_close($db);
+if(empty($_POST["password"]) || empty($_POST["username"])) {
 
-//if(empty($_POST["password"]) && empty($_POST["username"])) {
-
- //   echo "YOU CANNOT LEAVE BOTH INPUT FIELDS BLANK!. ";
-//}
+    echo "YOU CANNOT LEAVE BOTH INPUT FIELDS BLANK!. ";
+}
 
