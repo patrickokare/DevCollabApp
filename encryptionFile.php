ddc1d32423ee807b6411b102c36d13430033bd10
waleokare <waleokare@outlook.com>
2016-08-18 00:33:24 +0100

    encryption

--- encryptionFile.php
+++ encryptionFile.php
@@ -1,70 +1,29 @@
 <?php
+session_start();
+include "connection.php";
 
-include_once "connection.php";
-//require 'session.php';
-//$username = $_POST['username'];
-$key = md5('australia');
-$salt = md5('australia');
-
-
-function encrypt($string, $key){
-    $string = rtrim(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $string, MCRYPT_MODE_ECB)));
-    return $string;
-}
+$username = $_POST['username'];
+$password= $_POST['password'];
 
 
-function decrypt($string, $key){
-    $string = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, base64_decode($string),MCRYPT_MODE_ECB));
-    return $string;
-}
-
-function hashword($string, $salt){
-    $string = crypt($string, '$1$'. $salt . '$');
-    return $string;
-}
+$sql = "SELECT * FROM users WHERE username = '$username'";
+$result = $db->query($sql);
+$row = $result->fetch_assoc();
+$hash_password = $row['password'];
+$hash = password_verify($password, $hash_password);
 
 
-    $username = protect($_POST['username']);
-    $password = protect($_POST['password']);
-
-function protect($string){
-
-
-    $string = mysqli_real_escape_string(trim(strip_tags(addslashes($string))));
-    return $string;
-}
-
-
-
-if($_POST['submit'] == 'submit') {
-    if ($username == NULL) {
-     //   echo 'Enter a Username';
-    } else {
-        if ($password == NULL) {
-        //    echo 'Enter a Password';
-        } else {
-            $password = hashword($password, $salt);
-
-
-            $query = "INSERT INTO users (username,password)
-                            VALUES ('" . encrypt($username, $key) . "', '" . encrypt($password, $key) . "');";
-
-
-            if (mysqli_query($db, $query)) {
-                header('Location: success.php?username=' . $username);
-            } else {
-                echo 'Sorry, We Could not register you at this time. Try again Later.';
-            }
-
-        }
-
+if($hash ==0){
+    header("location: index.php?error=$error");
+    exit();
+}else{
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password='$hash_password'";
+    $result = $db->query($sql);
+    if(!$row = $result->fetch_assoc()){
+        echo "Your username or password is incorrect!";
+    }else{
+        $_SESSION['login_user'] = $row['username'];
 
     }
-
-
+    header('Location: success.php?username=' . $username);
 }
-
-
-
-
-
