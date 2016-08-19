a30811237a3e5fe8da37b325d78f6450399c900c
waleokare <waleokare@outlook.com>
2016-08-19 13:15:17 +0100

    md5

--- Home.php
+++ Home.php
@@ -6,49 +6,6 @@ if(!isset($_SESSION['login_user'])){
 }
 ?>
 
-<?php
-
-// This is a boolean that only equals true when teh  page is reloaded
-
-
-if (isset($_POST['submit'])) {
-    $selectedValue = $_POST['color'];
-    $userfilename = $selectedValue;
-    $oldfile = file_get_contents("http://karetechapp.azurewebsites.net/usercodes/" . $selectedValue);
-    $comment = $oldfile;
-
-    echo 'post if (2) statement';
-
-}else {
-
-    //$comment = null;
-    $userfilename = $_SESSION['login_user'] . '_' . 'codes.txt';
-    $oldfile = file_get_contents("http://karetechapp.azurewebsites.net/usercodes/" . $userfilename);
-    $comment = $oldfile;
-
-    echo 'post 1';
-}
-
-
-
-
-// when the form is submitted this code below will run
-if($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['preview-form-comment'])) {
-
-                      date_default_timezone_set('Europe/London');
-                      $today = date("Y-m-d H:i:s");
-
-         $comment = $_POST['preview-form-comment'] . "\r\n" . 'Codes Saved by:' .$_SESSION['login_user'] .' at '  . $today ;
-
-     $content = $comment;
-    $fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/usercodes/".$userfilename,"wb");
-    fwrite($fp,$content);
-
-    fclose($fp);
-
-}
-
-?>
 
 
 
