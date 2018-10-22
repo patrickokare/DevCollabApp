55410deedaed5d85c39fbe8f245f16aa83c15ddd
Babawale Patrick Okare <b.p.d.okare@rgu.ac.uk>
2018-10-22 18:46:53 +0100

    Update session.php

--- session.php
+++ session.php
@@ -1,26 +1,35 @@
 <?php
-include ("Connection.php");
 session_start();
+include ("Connection.php");
+
+
 $user_check = $_SESSION['login_user'];
+
+
 $ses_sql = mysqli_query($db," select username
                                        from users
                                        where username = '$user_check'
                                        ");
+
+
 $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-$login_session = $row['username']; // This displays the username the client or user type's in .......
 
+$login_session = $row['username']; // This displays the username the client or user type's in the home.php
+
+
+if(!isset($_SESSION['login_user'])){
+    header("location:login.php");
+}
+else{
+    header("location: index.php");
+}
+
+/**
 function loggedin(){
-
     if(!isset($_SESSION['login_user'])){
         return true;
     } else {
         return false;
     }
-
 }
-
-/**
-if(!isset($_SESSION['login_user'])){
-    header("location:login.php");
-}
- **/
+ * **/
