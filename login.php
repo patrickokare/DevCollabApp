fbc0163e734aaf472ff26ef9ab359fec7104901b
Babawale Patrick Okare <b.p.d.okare@rgu.ac.uk>
2018-10-22 18:45:12 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,51 +1,33 @@
-
-
 <?php
-include ("connection.php");
 session_start();
-
-
-
+include ("connection.php");
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     // Username and password sent from form
     // To protect MySQL injection for Security purpose
-
-
     $username = mysqli_real_escape_string($db,$_POST['username']);
     $password = md5(mysqli_real_escape_string($db,$_POST['password']));
-
     $username = stripslashes($username);
     $password = stripslashes($password);
-
     $_SESSION['login_user'] = htmlentities($_POST['username']);
-
     $sql = "SELECT uid
             FROM users
             WHERE username = '$username'
             and
             password = '$password'";
-
     $result = mysqli_query($db,$sql);
     $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
     $active = $row['active'];
     $count = mysqli_num_rows($result);
     // If result matched $username and $password, table row must be 1 row
-       if($count == 1) {
-       //error check here...........
-         $_SESSION['login_user'] = $username;
-
-
-         echo $_SESSION['login_user'];
+    if($count == 1) {
+        //error check here...........
+        $_SESSION['login_user'] = $username;
+        echo $_SESSION['login_user'];
         header("location: Home.php");// Redirecting To another Page
-        }else {
-           $error =  "<span style='color: darkred'> Username or Password Incorrect. Try Again! </span>";
-        header("location: index.php?error=$error");
-        echo $error;
+    }else {
 
+        header("location: index.php");
     }
-
 }
 mysqli_close($db);
-
-
