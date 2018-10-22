535bd7762429c1cbd5d435f49d8efe4c589dd15d
Babawale Patrick Okare <b.p.d.okare@rgu.ac.uk>
2018-10-22 18:50:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,109 +1,95 @@
+
 <?php
-
-$error2 = $_GET['error'];
-
+include ("connection.php");
 ?>
-
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="UTF-8">
-    <title>DevCollab Sign-In</title>
+    <title>DevCollab-Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
     <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrapmin.css">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <script src ="js/jquery.js"> </script>
     <script src="js/bootstrap.min.js"></script>
-<style>
-    h1{
-        color: darkred;
-    }
-span{
-    text-align: center;
-    color: darkred;
-}
-    input[type=submit]{
-        color: darkred; !important;
-    }
+    <style>
+                              a.nounderline {
 
-    body{
-    /    background-image: url("");
-     /   background-repeat: no-repeat;
-        max-width: 960px; !important;
-        margin-left: auto;!important;
-        margin-right: auto;!important;
-     /   background-color: transparent;
-    }
+                                  text-decoration: none;
+                                  color: silver;
+                              }
 
-</style>
+        h1{
+            color: #e74c3c;
+        }
+        span{
+            text-align: -webkit-center;
+            color: #e74c3c;
+        }
+        input[type=submit]{
+            color: silver; !important;
+        }
+        body{
+          /  max-width: 960px; !important;
+          /  margin-left: auto;!important;
+         /   margin-right: auto;!important;
+            background-color: #2ecc71;
+        }
+    </style>
 </head>
 
-
-<body>
 <header>
-
-
+    <br>
+    <br>
+    <br>
+    <br>
 
 </header>
-
+<body>
 
 <div class="modal-dialog">
-<div class="modal-content">
+    <div class="modal-content">
 
-    <div class="modal-header">
-        <h1 class="text-center" style="font-family: cursive"> DevCollab </h1>
+        <div class="modal-header">
+            <h1 class="text-center" style="font-family: cursive"> DevCollab </h1>
+            <div class="modal-body">
 
-
-    <div class="modal-body">
-<?php echo $error2; ?>
-        <form method="post" action="login.php" class="col-md-12 center-block">
-
-            <div class="form-group">
-
-                <input type="text" class="form-control input-lg" name="username" placeholder="Username" required>
-
+                <form method="POST" action="login.php" class="col-md-12 center-block">
+                    <div class="form-group">
+                        <input type="text" class="form-control input-lg" name="username" placeholder="Username" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control input-lg" name="password" placeholder="Password" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-block btn-lg btn-primary" value="Login">
+                    </div>
+                </form>
             </div>
 
-        <div class="form-group">
-
-    <input type="password" class="form-control input-lg" name="password" placeholder="Password">
-          </div>
-            <div class="form-group" style="color: darkred">
-    <input type="submit" class="btn btn-block btn-lg btn-primary" value="Login">
-
-           </div>
-
-        </form>
+        </div>
     </div>
 
 </div>
 
+<footer class="modal-dialog">
 
-</div>
-</div>
+    <hr style="color: #e74c3c">
 
-    <hr style="color: darkred">
+    <span style="font-family: cursive; color: #e74c3c;"> (c) DevCollab </span>
 
-    <footer class="site-footer">
+    <a href="egiser" class="nounderline"></a>
 
-        <div class="container">
-            <div class="row">
 
-                 <span style="font-family: cursive; color: darkred;"> (c) DevCollab App </span>
-                  <a href="egiser.php"> </a>
-
-            </div>
-
-            <div class="bottom-footer">
-
-            </div>
-        </div>
-    </footer>
-
+</footer>
 </body>
 
+
+
+
+
+
 </html>
-
-
